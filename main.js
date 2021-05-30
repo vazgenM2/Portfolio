f79ecba5829cab3070aacea423ce9d007f17b8b9
vazgenM2 <22vazgen22@gmail.com>
2021-05-30 16:21:05 +0300

    second

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ window.onload = function () {
         skill.addEventListener('mouseenter', () => {
             skill.style.boxShadow = `0px 0px 4px ${skill.getAttribute('data-color')}`
             skill.style.background = skill.getAttribute('data-color')
+            if (!skill.classList.contains('notwhite')) skill.style.color = '#fff'
             skill.style.boxShadow = `0px 0px 15px ${skill.getAttribute('data-color')}`
         })
         skill.addEventListener('mouseleave', () => {
             skill.style.background = 'none'
+            skill.style.color = '#000'
             skill.style.boxShadow = `0px 0px 4px ${skill.getAttribute('data-color')}`
         })
     }
@@ -20,7 +22,23 @@ window.onload = function () {
     })
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 0) document.querySelector('.top').style.background = 'linear-gradient(135deg, #111111, #121222)'
+        if (window.scrollY > document.querySelector('header').offsetHeight - document.querySelector('.top').offsetHeight) document.querySelector('.top').style.background = 'linear-gradient(135deg, #111111, #121222)'
         else document.querySelector('.top').style.background = 'linear-gradient(135deg, #11111190, #121222)'
     })
+
+    document.querySelectorAll('.nav-item').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            let href = this.getAttribute('href').substring(1);
+            const scrollTarget = document.getElementById(href);
+            const topOffset = document.querySelector('.top').offsetHeight;
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset - 25;
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
+
 }
